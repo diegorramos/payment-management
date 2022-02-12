0ff98fcbc9d399369228705630638528810a6d43
Diego Rocha <diego.ramos@willbank.com.br>
2022-02-12 19:11:31 -0300

    refactor: improve message send

--- payment/src/main/kotlin/br/com/diegorxramos/payment/infrastructure/notification/PaymentCreatedNotification.kt
+++ payment/src/main/kotlin/br/com/diegorxramos/payment/infrastructure/notification/PaymentCreatedNotification.kt
@@ -2,19 +2,18 @@ package br.com.diegorxramos.payment.infrastructure.notification
 
 import br.com.diegorxramos.payment.domain.model.Payment
 import br.com.diegorxramos.payment.infrastructure.enum.Exchange
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate
 import org.springframework.stereotype.Component
 
 @Component
-class PaymentCreatedNotification(private val amqpTemplate: RabbitMessagingTemplate) : PaymentSuccessNotification {
+class PaymentCreatedNotification(
+    private val objectMapper: ObjectMapper,
+    private val amqpTemplate: RabbitMessagingTemplate
+
+    ) : PaymentSuccessNotification {
 
     override fun notify(payment: Payment) {
-        val message = mapOf(
-            "date" to payment.date,
-            "paymentId" to payment.id,
-            "amount" to payment.amount,
-            "status" to payment.status
-        )
-        amqpTemplate.convertAndSend(Exchange.RECEIPT_EXCHANGE, message)
+        amqpTemplate.convertAndSend(Exchange.RECEIPT_EXCHANGE, objectMapper.writeValueAsString(payment))
     }
 }
