c65830944f5ef85889c9edd2aecf6fd4cb89e898
Diego Rocha <diego.ramos@willbank.com.br>
2022-02-12 17:42:37 -0300

    add: amqp configuration

---
+++ payment/src/main/kotlin/br/com/diegorxramos/payment/infrastructure/config/AmqpConfig.kt
@@ -0,0 +1,43 @@
+package br.com.diegorxramos.payment.infrastructure.config
+
+import br.com.diegorxramos.payment.infrastructure.enum.Exchange
+import org.springframework.amqp.core.AmqpAdmin
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AmqpConfig(private val amqpTemplate: RabbitMessagingTemplate) {
+
+    companion object {
+        const val EXCHANGE_DURABLE = true
+        const val EXCHANGE_AUTO_DELETE = false
+    }
+
+    @Bean
+    fun rabbitListenerContainerFactory(connectionFactory: ConnectionFactory): SimpleRabbitListenerContainerFactory {
+        return SimpleRabbitListenerContainerFactory().apply {
+            setConnectionFactory(connectionFactory)
+            setDefaultRequeueRejected(false)
+            setMissingQueuesFatal(false)
+            setConcurrentConsumers(2)
+            setMaxConcurrentConsumers(10)
+            setPrefetchCount(1)
+        }
+    }
+
+    @Bean
+    fun setupQueues(amqpAdmin: AmqpAdmin): InitializingBean {
+        return InitializingBean { declareTopic(amqpAdmin, Exchange.RECEIPT_EXCHANGE) }
+    }
+
+    private fun declareTopic(amqpAdmin: AmqpAdmin, exchangeName: String){
+        val args: Map<String, Any> = mapOf("x-delayed-type" to "topic")
+        val customExchange = TopicExchange(exchangeName, EXCHANGE_DURABLE, EXCHANGE_AUTO_DELETE, args)
+        amqpAdmin.declareExchange(customExchange)
+    }
+}
---
+++ payment/src/main/kotlin/br/com/diegorxramos/payment/infrastructure/enum/Exchange.kt
@@ -0,0 +1,6 @@
+package br.com.diegorxramos.payment.infrastructure.enum
+
+object Exchange {
+
+    const val RECEIPT_EXCHANGE = "payment.receipt.send"
+}
